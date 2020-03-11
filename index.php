5699a25d420716308c90a3eb0bb4f22ee9aafcba
Tetyana <tsergata@gmail.com>
2020-03-11 20:05:49 +0100

    Correzioni

--- index.php
+++ index.php
@@ -3,26 +3,29 @@
 
   // Dipendente N1
   $impiegato = new Dipendenti('Marco', 'Rossi', 'Impiegato');
-    echo $impiegato->_reparto = 'Amministrazione';
+    $impiegato->_reparto = 'Amministrazione';
     $impiegato->_orario = '9-16';
-    $impiegato->_RAL = 21.000 . ' ' . 'euro';
+    $impiegato->_RAL = 21000;
     $impiegato->_livello = 'Livello 5';
+    echo $impiegato->_reparto;
     var_dump($impiegato);
 
     // Dipendente N2
   $programmatore = new Dipendenti('Tania', 'Sergata', 'Programmatore');
-    echo $programmatore->_reparto = 'Informatico';
+    $programmatore->_reparto = 'Informatico';
     $programmatore->_orario = '9-18';
-    $programmatore->_RAL = 26.000 . ' ' . 'euro';
+    $programmatore->_RAL = 26000;
     $programmatore->_livello = 'Livello 4';
+    echo $programmatore->_reparto;
     var_dump($programmatore);
 
     // Dipendente N3
   $segretaria = new Dipendenti('Anna', 'Bianchi', 'Segretaria');
-    echo $segretaria->_reparto = 'Segreteria';
+    $segretaria->_reparto = 'Segreteria';
     $segretaria->_orario = '9-18';
-    $segretaria->_RAL = '20.000 euro';
+    $segretaria->_RAL = 20000;
     $segretaria->_livello = 'Livello 5';
+    echo $segretaria->_reparto;
     var_dump($segretaria);
 
 
